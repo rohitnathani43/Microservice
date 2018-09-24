86baa5c2ae5e87e6babe70f225a510cdb3fa3d87
Rohit Nathani <rohit.nathani91@gmail.com>
2018-09-25 00:06:33 +0530

    Removing unnecessary comment

--- MicroservicePOC/src/main/java/com/microservice/poc/MainApp.java
+++ MicroservicePOC/src/main/java/com/microservice/poc/MainApp.java
@@ -13,7 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class MainApp {
 
-    // Adding comment
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
     }
