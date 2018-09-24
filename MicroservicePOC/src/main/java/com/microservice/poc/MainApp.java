3290a070bde84eae6a7a075a21a48cc2a452c126
Rohit Nathani <rohit.jsp7@gmail.com>
2018-09-24 23:55:15 +0530

    Update MainApp.java

--- MicroservicePOC/src/main/java/com/microservice/poc/MainApp.java
+++ MicroservicePOC/src/main/java/com/microservice/poc/MainApp.java
@@ -13,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class MainApp {
 
+    // Adding comment
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
     }
